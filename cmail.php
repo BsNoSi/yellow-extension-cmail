d894175e9dd003656c7f976daf6012ef5a0559e6
BsNoSi <github@buoa.de>
2019-04-23 09:42:52 +0200

    Improvements: New API and Multilanguage

--- cmail.php
+++ cmail.php
@@ -2,54 +2,42 @@
 // cmail Plugin for YElLOW Copyright (c)2018 NoSi.de
 // YELLOW Copyright (c) 2013-2017 Datenstrom, http://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
-// EDIT THE SETUP FOR E-MAIL!
+// ***************************************************************
+// EDIT THE SETUP FOR E-MAIL IN EVERY TXT-FILE BELONGING TO CMAIL!
+// ***************************************************************
 
 class YellowCmail {
-    const VERSION = "1.0";
+    const VERSION = "1.1.0";
+    const TYPE = "feature";
     public $yellow;         //access to API
 
     // Handle initialisation
     public function onLoad($yellow) {
         $this->yellow = $yellow;
     }
-    
-    // Handle page content parsing of custom block
-    public function onParseContentBlock($page, $name, $text, $shortcut) {
+	   
+    // Handle page content of shortcut
+    public function onParseContentShortcut($page, $name, $text, $type) {
         $output = null;
-        if ($name=="cmail" && $shortcut) {
+        if ($name=="cmail") {
             list($subject, $m_link) = $this->yellow->toolbox->getTextArgs($text);
-            if (empty($subject)) $subject = $_SERVER['REQUEST_URI'];	
-			
-// Setup for e-mail
-			$m_adress = "mail@domain.tld";
-			$m_site  = "https//:www.domain.tld";
-			$m_subtitle = "preccessor subject: ";
-			$m_body = " (Please do not remove)\n\nHello!\n\n[Your Text]";
-			if (empty($m_link)) $m_link = "contact";
-// End setup
- 
-      $subject = $m_subtitle . $subject;
-
-
+            $subject = $subject ? $subject : $yellow->page->base . $_SERVER['REQUEST_URI'];
 			// modify to "mailto" compatible strings
-			$r_in = array("%", "\n", " ", "/", "!", "#",  "*", "<", ">", "?", "&");
-			$r_out = array("%25", "%0A", "%20", " %2F", "%21", "%23",  "%2A", "%3C", "%3E", "%3F", "%26");
+			$r_in =  array("%",   "/n",  " ",   "/",   "!",   "#",   "*",   "<",   ">",   "?",   "&");
+			$r_out = array("%25", "%0A", "%20", "%2F", "%21", "%23", "%2A", "%3C", "%3E", "%3F", "%26");
 			
-			$subject = str_replace($r_in, $r_out, $subject);
-			$m_body = str_replace($r_in, $r_out, $m_body);
-
-			$output = " <a href=mailto:" . $m_adress . "?subject=" . $subject;
-			$output .= "&body=Backlink:%20" . $m_site . $yellow->page->base . $_SERVER['REQUEST_URI'];
-			$output .= $m_body . ">";
-			$output .= htmlspecialchars($m_link) . "</a> ";
+			$subject = str_replace($r_in,$r_out,$this->yellow->text->get("cmail_subtitle") ." ". $subject);
+			$output = " <a href=mailto:" . $this->yellow->text->get("cmail_adress") . "?subject=" . $subject;
+			$output .= "&body=Backlink:%20" . $this->yellow->text->get("cmail_site") . $yellow->page->base . $_SERVER['REQUEST_URI'];
+			$output .= str_replace($r_in, $r_out,$this->yellow->text->getHtml("cmail_body")) . ">";
+			$output .= htmlspecialchars($m_link ? : $this->yellow->text->getHtml("cmail_link")) . "</a> ";
         }
+	
         return $output;
     }
-
-    // Handle page extra HTML data
-    public function onExtra($name) {
-        return $this->onParseContentBlock($this->yellow->page, $name, "", true);
-    }
+   // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        return $this->onParseContentShortcut($page, $name, "", "block");
+		
+    }	
 }
-
-$yellow->plugins->register("cmail", "YellowCmail", YellowCmail::VERSION);
