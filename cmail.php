c116d0ed217f6a3bd9ae7a242568cc7fc427de11
BsNoSi <nosi@nosi.de>
2020-10-13 12:34:04 +0200

    Update 1.6

--- cmail.php
+++ cmail.php
@@ -3,13 +3,14 @@
 // Copyright ©2018-now Norbert Simon, https://nosi.de for
 // YELLOW Copyright ©2013-now Datenstrom, http://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
+// requires YELLOW 0.8.15 or higher
 // ***************************************************************
 // SEE README.MD FOR SETUP REQUIREMENTS
 // ***************************************************************
 
 class YellowCmail {
-    const VERSION = "1.5.1";
-    const TYPE = "feature";
+    const VERSION = "1.6";
+    
     //access to API
     public $yellow;         
     // Handle initialisation
@@ -20,15 +21,15 @@ class YellowCmail {
     public function onParseContentShortcut($page, $name, $text, $type) {
         $output = null;
         if ($name=="cmail") { 
-            list($subject, $link, $title, $lng) = $this->yellow->toolbox->getTextArguments($text);
-		$lng = empty($lng) ? $this->yellow->text->get("language") : $lng;
-		$subject = $subject ? $subject : $this->yellow->text->getText("cmail_std", $lng); 
+            list($subject, $link, $title, $lng) = $this->yellow->toolbox->getTextArgs($text);
+		$lng = empty($lng) ? $this->yellow->language->getText("language") : $lng;
+		$subject = $subject ? $subject : $this->yellow->language->getText("cmail_link", $lng); 
 		$link = $link ? $link : $subject;
 		$title = $title ? $title : $subject;
-		$lng = $lng ? "&l=". $lng : "" ;
+		$lng = $lng ? $lng . "-" : "" ;
 		$subject = rawurlencode($subject);
-		$output = " <a class=\"cmail\" href=\"/xtra/cmail?s=" . $subject . $lng;
-		$output .= "\" title=\"" . $title ."\">";
+		$output = " <a class=\"cmail\" target=\"_blank\" href=\"/xtra/cmail?q=" . $subject . "&l=" . $lng;
+		$output .= "\" title=\"" . $title . "\">";
 		$output .= htmlspecialchars($link) . "</a> ";
         }
         return $output;
